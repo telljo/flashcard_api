290c85ff4fb02b9fffa22b3922112e321c45b75a
Josh Tell <jtell1997@gmail.com>
2025-10-23 16:55:01 +0800

    Add missing controller methods

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -11,7 +11,24 @@ class DecksController < ApplicationController
     render json: @deck
   end
 
-  def destroy; end
+  def destroy
+    if @deck.destroy
+      head :no_content
+    else
+      render_erro(messages: @deck.errors.full_messages, status: :unprocessable_entity)
+    end
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+    @deck.user = Current.user
+
+    if @deck.save!
+      render json: @deck
+    else
+      render_error(messages: @deck.errors.full_messages, status: :unprocessable_entity)
+    end
+  end
 
   def update
     if @deck.update(deck_params)
